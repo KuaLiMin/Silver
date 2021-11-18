ec917b9120d44f85fa9b56beffd578f71a80badd
Li Min Kua <s10206234@connect.np.edu.sg>
2021-11-18 10:30:45 +0800

    Added fin mechanism

--- js/finarticles.js
+++ js/finarticles.js
@@ -15,7 +15,8 @@ function finNews() {
          .then(function(data){  
          var channel = data.querySelectorAll("channel")[0]
          //$("#update").text(channel.querySelectorAll("lastBuildDate")[0].innerHTML) /*change updated date*/
-         console.log(channel)
+
+
          if (channel != null)
          {
          /*update articles*/
@@ -74,8 +75,6 @@ function finNews() {
                   news.push([tit, lin])
                   /*create articles*/
 
-                  console.log(tim, suff, tit, pic, lin)
-
                   $(".theNews").append(`
                   <button  onclick="window.open('`+lin+`', '_blank')" class="card cardicle p-0 border-0 mb-3" style="background-color: var(--clrBgAccentSoft);">
                   <div class="row no-gutters">
@@ -109,9 +108,24 @@ function finNews() {
 tutorialArticles()
 function tutorialArticles(){
     articles = {
-        "article one": "abc abc abc",
-        "article two": "abc abc abc"
+        "art1": ["This is a title", `<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently <a><u>with desktop</u></a> publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> <img class='col-12' src="https://www.himgs.com/imagenes/hello/social/hello-fb-logo.png"/> <p>another paragraph</p>`],
+        "art2": ["title", "story"]
     }
 
-    
+
+    $(".lessonsList").empty()
+
+    for (const [key, value] of Object.entries(articles)) {
+      $(".lessonsList").append(`<button id="`+ key +`" class="cardicle finLesson list-group-item bg-transparent border-0">`+ value[0]+`</button>`)
+    }
 }
+
+
+$('body').on('click', '.finLesson', function() {
+   id = jQuery(this).attr("id")
+
+   $(".modal-title").text(articles[jQuery(this).attr("id")][0])
+   $(".modal-body").empty()
+   $(".modal-body").append(articles[jQuery(this).attr("id")][1])
+   $('#newsModal').modal('show')
+});
