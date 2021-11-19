80a673c2a50b1e6189d814d1dc299a0e1de149ca
ArrickTee <s10198673@connect.np.edu.sg>
2021-11-19 08:21:19 +0800

    code for set goals as well as ref for buttons

--- js/auth.js
+++ js/auth.js
@@ -1,11 +1,5 @@
-//getting goals
-
-
-
-
-
 //listen for auth status changes
-auth.onAuthStateChanger(user => {
+auth.onAuthStateChanged(user => {
     if (user) {
         console.log('user logged in :', user)
             /*db.collection('goals').get().then(snapshot => {
@@ -19,12 +13,12 @@ auth.onAuthStateChanger(user => {
     }
 });
 
-//creating new goals
-const createForm = document.querySelector('whatever the id is suppose to be');
-createForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    //add code related to adding goals
-});
+// //creating new goals
+// const createForm = document.querySelector('whatever the id is suppose to be');
+// createForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     //add code related to adding goals
+// });
 
 
 
@@ -32,18 +26,12 @@ createForm.addEventListener('submit', (e) => {
 
 
 
-
-
-
-
-
-
-//logout function
-const logout = document.querySelector('whatever the logout button is');
-logout.addEventListener('click', (e) => {
-    e.preventDefault();
-    auth.signOut().then(() => {});
-});
+// //logout function
+// const logout = document.querySelector('whatever the logout button is');
+// logout.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     auth.signOut().then(() => {});
+// });
 
 //login function (not google rn)
 const email = 'arricktee2002@yahoo.com';
